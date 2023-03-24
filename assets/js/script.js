a7cba8497cf682e9489f1a41bcb4cabab36ffa1d
rocas3096 <ozavala3096@gmail.com>
2023-03-24 14:48:49 -0500

    Created starter code for css, java and html. Added starter functions for timer in java.

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var timerElement = document.querySelector(".timer-count")
+var startButton = document.querySelector(".start-button")
+
+
+var timer;
+var timerCount;
+
+function startGame(){
+    timerCount=100
+    startButton.disabled = true;
+    startTimer()
+}
+
+function startTimer() {
+    timer = setInterval(function (){
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >=0) {
+            if (finishQuestions & timerCount > 0) {
+                clearInterval(timer);
+                finishQuestions();
+            }
+        }
+        if (timerCount === 0) {
+            clearInterval(timer);
+            timeUp();
+        }
+    }, 1000)
+}
+
+startButton.addEventListener("click", startGame)
+
+
+var playagainbutton = document.querySelector(".play-again-button");
+
+function resetGame() {
+    setHighscores()
+}
+
+playagainbutton.addEventListener("click", resetGame);
